c56172eea85b0cefb2999c8a1170352f64a66ce5
Hunter Goram <huntergoram2@gmail.com>
2024-01-27 17:04:12 -0600

    Support for screens.

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+      
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen

f7637df2a363ca8d295c0e6832b218d431eaf7c5
Hunter Goram <huntergoram2@gmail.com>
2024-02-05 01:22:42 -0500

    Add implementation for user profile and token setting endpoints.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,16 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d'
-    });
-
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production' ? true : false,
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000 // 1 day in milliseconds
-    });
+    generateAndSetToken(res, user._id); // Set token in cookie
 
     res.json({
       _id: user._id,
@@ -49,16 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = User.create({ name, email, password });
 
   if (user) {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d'
-    });
-
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production' ? true : false,
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days in milliseconds
-    });
+    generateAndSetToken(res, user._id); // Set token in cookie
 
     res.status(201).json({
       _id: user._id,
@@ -77,11 +59,45 @@ const deleteUser = asyncHandler(async (req, res) => {
 })
 
 const updateUser = asyncHandler(async (req, res) => {
-  res.send('Update user');
+  const user = User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = user.save();
+
+    generateAndSetToken(res, updatedUser._id); // Set token in cookie
+
+    res.status(201).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 })
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Get user profile');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 const getUsers = asyncHandler(async (req, res) => {
---
+++ backend/utils/authUtils.js
@@ -0,0 +1,19 @@
+
+
+const generateToken = (userId, duration) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: duration,
+  });
+}
+
+const generateAndSetToken = (res, userId, duration = '7 days') => {
+  const token = generateToken(userId, duration);
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+  });
+
+  return token;
+} 

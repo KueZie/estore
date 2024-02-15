6932d7c26542fe3bcb5f3fd0949f448ef278b574
Hunter Goram <huntergoram2@gmail.com>
2024-02-15 13:29:38 -0500

    Fix formatting.

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -8,24 +8,10 @@ const orderSchema = new mongoose.Schema({
   },
   orderItems: [
     {
-      name: {
-        type: String,
-        required: true
-      },
-      qty: {
-        type: Number,
-        required: true,
-        default: 0
-      },
-      image: {
-        type: String,
-        required: true
-      },
-      price: {
-        type: Number,
-        required: true,
-        default: 0
-      },
+      name: { type: String, required: true },
+      qty: { type: Number, required: true, default: 0 },
+      image: { type: String, required: true},
+      price: { type: Number, required: true, default: 0 },
       product: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
@@ -34,22 +20,10 @@ const orderSchema = new mongoose.Schema({
     }
   ],
   shippingAddress: {
-    address: {
-      type: String,
-      required: true
-    },
-    city: {
-      type: String,
-      required: true
-    },
-    postalCode: {
-      type: String,
-      required: true
-    },
-    country: {
-      type: String,
-      required: true
-    }
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true }
   },
   paymentMethod: {
     type: String,

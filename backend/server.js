d336c5038c472d6678a253ad8f22e52c1f13171e
Hunter Goram <huntergoram2@gmail.com>
2024-02-15 13:15:11 -0500

    Add basic order backend routes.

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -29,6 +30,7 @@ app.get('/', (req, res) => {
 // Use productRoutes
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use(notFound); 
 app.use(errorHandler); 

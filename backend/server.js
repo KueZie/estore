28efbc269f9d70ba893e9bc60032d7040fcdcd6b
Hunter Goram <huntergoram2@gmail.com>
2024-02-21 23:12:30 -0500

    Add backend endpoint for image uploads.

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import { notFound, errorHandler } from './middleware/errorHandler.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import uploadRouter from './routes/uploadRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -31,8 +32,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/upload', uploadRouter);
 
 app.use('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID })); // PayPal Client ID
+
+const __dirname = path.resolve(); // Set __dirname to the current working directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // Make uploads folder static
 
 app.use(notFound); 
 app.use(errorHandler); 
